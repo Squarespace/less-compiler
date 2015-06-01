b52af4d8c48bf7b131cec105219db23824c50cb1
Patrick Hensley <pathensley@gmail.com>
2015-06-01 15:16:16 -0400

    Split less compiler into subprojects.

--- conf/license.sh
+++ conf/license.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-CWD=$(cd `dirname $0`; pwd)
-GIT=$CWD/..
+ROOT=$(cd `dirname $0`/..; pwd)
+HEADER=${ROOT}/conf/apache-2.0-header.txt
+PROJECTS="
+    $ROOT/less-cli
+    $ROOT/less-compiler
+"
 
-for f in `find $GIT/src -name '*.java'` ; do
-    if ! grep -q '* Licensed under the Apache License' $f ; then
-        cat $CWD/apache-2.0-header.txt $f >$f.new && mv $f.new $f
-    fi
+for project in ${PROJECTS} ; do
+    for f in `find ${project}/src -name '*.java'` ; do
+        if ! grep -q '* Licensed under the Apache License' $f ; then
+            cat ${HEADER} $f >$f.new && mv $f.new $f
+        fi
+    done
 done
+
 
